fe5ecdc95c269dd427168766d8946135d5598249
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-01-20 21:15:18 +0100

    Complete repeatString function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function repeatStr(string, num) {
 
+    // Preventing negative number of repeat
+    if (num < 0) {
+        return 'ERROR'
+    }
+
+    let repeatedString = '';
+
+    for (let i = 0; i < num; i++) {
+        repeatedString += string;
+    }
+    
+    return repeatedString
 };
 
 // Do not edit below this line

a2f671dd30c498d731ad8994f9f537508c592d92
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-01-22 20:25:53 +0100

    Create the expected temperature conversion funcs

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function convertToCelsius(farheinheitTemp) {
+  let temp = parseFloat(((farheinheitTemp - 32) * 5/9).toFixed(1));
+  
+  if (temp === parseInt(temp)) {
+    return Math.floor(temp)
+  } else {
+    return temp
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function convertToFahrenheit(celsiusTemp) {
+  let temp = parseFloat((celsiusTemp * 9/5 + 32).toFixed(1));
+    if (temp === parseInt(temp)) {
+    return Math.floor(temp)
+  } else {
+    return temp
+  }
 };
 
 // Do not edit below this line
